40b41ab1d549886052ee864f8489e002f712b812
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:12:58 +0500

    Create categories.js for income and expense categories

---
+++ js/categories.js
@@ -0,0 +1,28 @@
+// categories.js
+// Категории доходов и расходов
+
+const categories = (() => {
+
+    const render = () => {
+        // Отрисовка категорий
+    };
+
+    const add = (data) => {
+        // Добавить категорию
+    };
+
+    const remove = (id) => {
+        // Удалить категорию
+    };
+
+    const update = (id, data) => {
+        // Редактировать категорию
+    };
+
+    return {
+        render,
+        add,
+        remove,
+        update
+    };
+})();
