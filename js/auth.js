4e06462fe999ce02e875cf74a55881ef21c36b81
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:12:04 +0500

    Add authentication module with PIN protection

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+// auth.js
+// PIN-защита и авторизация
+
+const auth = (() => {
+
+    const init = () => {
+        // Проверка PIN при запуске
+    };
+
+    const checkPin = (pin) => {
+        // Проверка PIN
+        return true;
+    };
+
+    const setPin = (pin) => {
+        // Установка нового PIN
+    };
+
+    return {
+        init,
+        checkPin,
+        setPin
+    };
+})();
