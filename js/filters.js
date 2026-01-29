eec0f5f9112a76b7f25cbd7f43c13300f1ab1365
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:15:18 +0500

    Add filters module for date and text operations

---
+++ js/filters.js
@@ -0,0 +1,23 @@
+// filters.js
+// Фильтрация, поиск, сортировка
+
+const filters = (() => {
+
+    const byDate = (from, to) => {
+        // Фильтр по дате
+    };
+
+    const byText = (text) => {
+        // Поиск по операциям
+    };
+
+    const sort = (type) => {
+        // new | old
+    };
+
+    return {
+        byDate,
+        byText,
+        sort
+    };
+})();
