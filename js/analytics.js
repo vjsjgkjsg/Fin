bc51ab9388dd2c30bce217fbc21641aca8277828
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:14:30 +0500

    Add analytics module for charts and statistics

---
+++ js/analytics.js
@@ -0,0 +1,19 @@
+// analytics.js
+// Аналитика и графики
+
+const analytics = (() => {
+
+    const renderCharts = () => {
+        // График расходов
+    };
+
+    const calculateStats = () => {
+        // Статистика по категориям
+        return {};
+    };
+
+    return {
+        renderCharts,
+        calculateStats
+    };
+})();
