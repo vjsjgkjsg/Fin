084907188070738ca0cd4f3391d53643e8e3a338
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:14:56 +0500

    Add ui.js for interface and event handling

---
+++ js/ui.js
@@ -0,0 +1,23 @@
+// ui.js
+// Весь интерфейс и события
+
+const ui = (() => {
+
+    const init = () => {
+        // Навешиваем события
+    };
+
+    const openModal = (id) => {
+        // Открыть модалку
+    };
+
+    const closeModal = (id) => {
+        // Закрыть модалку
+    };
+
+    return {
+        init,
+        openModal,
+        closeModal
+    };
+})();
