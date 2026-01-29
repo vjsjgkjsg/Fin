91228c8a0e9f97e245c8327c2911742994a8c51a
vjsjgkjsg <goodw21de@gmail.com>
2026-01-29 23:13:27 +0500

    Create records.js for income/expense operations

---
+++ js/records.js
@@ -0,0 +1,28 @@
+// records.js
+// Операции (доходы / расходы)
+
+const records = (() => {
+
+    const add = (record) => {
+        // Добавить запись
+    };
+
+    const remove = (id) => {
+        // Удалить запись
+    };
+
+    const update = (id, data) => {
+        // Редактировать запись
+    };
+
+    const getAll = () => {
+        return [];
+    };
+
+    return {
+        add,
+        remove,
+        update,
+        getAll
+    };
+})();
